3ea2515491f412d6cc9a6b6185074e0121248343
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-02-24 17:37:42 +0200

    change saturate in mobile view

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -33,7 +33,7 @@ export const Landing = ({ setSelectedPage }: Props) => {
 				) : (
 					<img
 						src='../assets/profile-image.png'
-						className='hover:filter hover:saturate-200 transition duration-500 z-10 w-full
+						className='hover:filter hover:saturate-50 transition duration-500 z-10 w-full
 					max-w-[400px] md:max-w-[600px]'
 						alt='Profile'
 					/>
